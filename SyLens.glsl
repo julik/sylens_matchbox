812b34a2b3d9caec94b5d1968210b2fe3581234e
Julik <me@julik.nl>
2012-01-18 18:00:58 +0100

    Dicking about

--- SyLens.glsl
+++ SyLens.glsl
@@ -10,19 +10,20 @@
 */
 
 uniform sampler2D input1;
-uniform float adsk_input1_w, adsk_input1_h, adsk_input1_frameratio;
+uniform float adsk_input1_w, adsk_input1_h, adsk_input1_aspect;
 uniform float kCoeff, kCube;
 uniform float adsk_result_w, adsk_result_h;
 uniform bool doRedisto;
 
-vec2 redisto(vec2 pt) {
+vec2 redisto(vec2 pt, float r2) {
   float r, rp, f, rlim, rplim, raw, err, slope;
+  float _aspect = adsk_input1_w / adsk_input1_h;
   vec2 res;
   
-  rp = sqrt(adsk_input1_frameratio * adsk_input1_frameratio * pt.x * pt.x + pt.y * pt.y);
+  rp = sqrt(_aspect * _aspect * pt.x * pt.x + pt.y * pt.y);
   if (kCoeff < 0.0) {
     rlim = sqrt((-1.0/3.0) / kCoeff);
-    rplim = rlim * (1.0 + kCoeff*rlim*rlim);
+    rplim = rlim * (1 + kCoeff*rlim*rlim);
     if (rp >= 0.99 * rplim) {
       f = rlim / rp;
       return pt * f;
@@ -32,47 +33,51 @@ vec2 redisto(vec2 pt) {
   r = rp;
   for (int i = 0; i < 20; i++) {
     raw = kCoeff * r * r;
-    slope = 1 + 3.0*raw;
-    err = rp - r * (1.0 + raw);
+    slope = 1 + 3*raw;
+    err = rp - r * (1 + raw);
     if (abs(err) < 0.00001) break;
     r += (err / slope);
   }
   f = r / rp;
   
-  return pt * f;
+  // For the pixel in the middle of the image the F can
+  // be NaN, so check for that and leave it be.
+  // http://stackoverflow.com/questions/570669/checking-if-a-float-or-float-is-nan-in-c
+  // If a NaN F does crop up it creates a black pixel in the image - not something we love
+  if(f == f) {
+    res = pt * f;
+  }
+  return res;
 }
 
-vec2 undisto(vec2 pt) {
-  // We compute the distortion based on the radius off the center, squared.
-  // Therefore R2.
-  float r2, f;
-  f = adsk_input1_frameratio * adsk_input1_frameratio * pt.x * pt.x + pt.y * pt.y;
+vec2 undisto(vec2 pt, float r2) {
+  float f;
   // Skipping the square root speeds up things
   if (abs(kCube) > 0.00001) {
-    f = 1.0 + r2*(kCoeff + kCube * sqrt(r2));
+    f = 1 + r2*(kCoeff + kCube * sqrt(r2));
   } else {
-    f = 1.0 + r2*(kCoeff);
+    f = 1 + r2*(kCoeff);
   }
   return pt * f;
 }
 
 void main(void)
 {
-   vec2 uv = gl_FragCoord.xy / vec2(adsk_input1_w, adsk_input1_h);
+   vec2 uv;
+   // This is a coordinate in the OUTPUT, we however need a coord in the input. Hmm.
+   uv = gl_FragCoord.xy / adsk_result_w;
+   float _aspect = adsk_input1_w / adsk_input1_h;
    
-   // Syntheyes operates in coordinates off-center of the image
    uv = uv - 0.5;
+   float r2 = _aspect * _aspect * uv.x * uv.x + uv.y * uv.y;
    if(doRedisto) {
-     uv = undisto(uv);
+     uv = undisto(uv, r2);
    } else {
-     uv = redisto(uv);
+     uv = redisto(uv, r2);
    }
-   // Move to the lower left corner which is where the texture starts
+   // Back to off-corner
    uv = uv + 0.5;
    
-   // TODO - compensate for the changing plate size in the output w/r to the input
-   
-   // Perform sampling, TODO - also sample from alpha in the second input
    vec4 tex0 = texture2D(input1, uv);
    gl_FragColor.rgba = vec4(tex0.rgb, 1.0 );
 }
