4ecf52b4984a2ccee36b86612cb0bee62f2a0373
Julik <me@julik.nl>
2012-04-17 10:51:53 +0200

    Add centerpoint shifts

--- SyLens.glsl
+++ SyLens.glsl
@@ -12,14 +12,15 @@
 */
 
 // Controls
-uniform float kCoeff, kCube;
+uniform float kCoeff, kCube, uShift, vShift;
+uniform bool apply_disto;
 
 // Uniform inputs
 uniform sampler2D input1;
 uniform float adsk_input1_w, adsk_input1_h, adsk_input1_aspect;
 uniform float adsk_result_w, adsk_result_h;
 
-float distoF(float r) {
+float distortion_f(float r) {
   float f;
   float r2 = r * r;
   f = 1 + r2*(kCoeff + kCube * sqrt(r2));
@@ -40,7 +41,7 @@ float inverse_f(float r)
     float lut_r = 0;
     float f;
     for(int i=0; i < 32; i++) {
-        f = distoF(lut_r);
+        f = distortion_f(lut_r);
         lut[i] = vec3(lut_r, f, lut_r * f);
         lut_r += incr;
     }
@@ -77,8 +78,17 @@ void main(void)
    uv.y = px.y / adsk_input1_h;
    
    uv -= 0.5;
+   
+   uv.x -= uShift;
+   uv.y -= vShift;
    r = sqrt((uv.x*uv.x) +  (uv.y * uv.y * adsk_input1_aspect * adsk_input1_aspect));
-   uv = uv * distoF(r);
+   if(apply_disto){
+       uv = uv * inverse_f(r);  
+   } else {
+       uv = uv * distortion_f(r);  
+   }
+   uv.x += uShift;
+   uv.y += vShift;
    
    uv += 0.5;
    
