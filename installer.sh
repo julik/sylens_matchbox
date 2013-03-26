526f1d1302a5ffb80629920019a7b38293034f3c
Julik Tarkhanov <me@julik.nl>
2013-03-26 10:23:34 +0100

    RHEL complains on the argument position to $find

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 # Find all Matchbox directories
 echo "Installing SyLens Matchbox. I will need your sudo password for this (or run the script as root if your IFFS user is not a sudoer)."
 
-find /usr/discreet/* -name "matchbox" -maxdepth 1 | \
+find /usr/discreet/* -maxdepth 1 -name "matchbox" | \
 while read line
 do
   echo "Copying the shaders to $line/"
